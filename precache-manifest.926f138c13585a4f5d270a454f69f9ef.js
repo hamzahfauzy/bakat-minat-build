0ff5c89e357ec32ed1e364d6fa8848851874c266
hamzahfauzy <hamzahfauzy97@gmail.com>
2020-07-28 20:14:13 +0700

    first commit

---
+++ precache-manifest.926f138c13585a4f5d270a454f69f9ef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "064a6f7d8856eabdb3b6",
+    "url": "/css/app.00a4d7b2.css"
+  },
+  {
+    "revision": "6950d0712c22ec798ae3",
+    "url": "/css/chunk-vendors.03c4158b.css"
+  },
+  {
+    "revision": "fadc7e7eab9f87b94c7cfc10dd385f59",
+    "url": "/img/logo.fadc7e7e.png"
+  },
+  {
+    "revision": "fe560772d5c9fc17d784c9c7c6cd443a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "064a6f7d8856eabdb3b6",
+    "url": "/js/app.e614390c.js"
+  },
+  {
+    "revision": "6950d0712c22ec798ae3",
+    "url": "/js/chunk-vendors.4e1f87f9.js"
+  },
+  {
+    "revision": "ed53ef2d6f1a8197e925a6caf8069193",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
